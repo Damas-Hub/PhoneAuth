f0e111875ed879cdd39de3c7de13a315adae3ae3
Selormey Hubert <selormeyh@gmail.com>
2023-10-01 05:42:17 +0000

    otp auth

---
+++ config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDz1WLEElL73007SBdGnuxBxBUW0UfggIg",
+    authDomain: "phoneauth-325f4.firebaseapp.com",
+    projectId: "phoneauth-325f4",
+    storageBucket: "phoneauth-325f4.appspot.com",
+    messagingSenderId: "383897624982",
+    appId: "1:383897624982:web:fd579906b4a5f6fee7fdb7",
+    measurementId: "G-S7XEZE3Q8P"
+   };
+  
+ 
+   
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  
+   
